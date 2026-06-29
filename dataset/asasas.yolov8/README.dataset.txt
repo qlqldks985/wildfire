7091e9bb94cd93b76d774e321da1852dd9aa5d5a
qlqldks985 <qlqldks985@gmail.com>
2026-06-29 17:15:28 +0900

    0629 esp32 작업

--- dataset/asasas.yolov8/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# asasas > asasas-smyju
-https://universe.roboflow.com/-r59va/asasas-smyju
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import time
 
 # ====================== 설정 ======================
 # 모델 경로 (현재 당신 프로젝트 구조에 맞춤)
-MODEL_PATH = 'runs/segment/fire_smoke-3/weights/best.pt' 
+# main.py 상단 수정 예시
+MODEL_PATH = 'runs/detect/train/weights/best.pt' 
 
-ESP32_URL = "http://192.168.137.72:81/stream"
-CONFIDENCE_THRESHOLD = 0.75
+ESP32_URL = "http://192.168.137.178:81/stream"
+CONFIDENCE_THRESHOLD = 0.20
 
 # ☀️ 렌즈 밝기 및 대비 설정 (수정 가능한 구역)
 # BRIGHTNESS: -255 ~ 255 (음수면 어두워지고, 양수면 밝아집니다)
 # CONTRAST: 0.5 ~ 3.0 (1.0이 기본값, 높일수록 명암 대비가 강해집니다)
-BRIGHTNESS = -30    # 💡 팁: 창문 빛 번짐을 줄이려면 약간 음수(-20 ~ -50)로 낮춰보세요!
-CONTRAST = 1.2      # 💡 팁: 대비를 살짝 높이면 연기와 빛의 경계가 뚜렷해집니다.
+BRIGHTNESS = 0    # 💡 팁: 창문 빛 번짐을 줄이려면 약간 음수(-20 ~ -50)로 낮춰보세요!
+CONTRAST = 1.0      # 💡 팁: 대비를 살짝 높이면 연기와 빛의 경계가 뚜렷해집니다.
 # =================================================
 # =================================================
 
@@ -32,10 +33,12 @@ if not cap.isOpened():
     print("❌ ESP32 연결 실패!")
     print("   1. ESP32가 WiFi에 잘 연결되어 있는지")
     print("   2. IP 주소 확인 (192.168.137.72)")
-    print("   3. 브라우저에서 http://192.168.137.72:81/stream 열리는지 테스트")
+    print("   3. 브라우저에서 http://192.168.137.178:81/stream 열리는지 테스트")
     exit()
 
 print("✅ ESP32 카메라 연결 성공!")
+
+start_time = time.time()
 
 while True:
     ret, frame = cap.read()
@@ -47,7 +50,7 @@ while True:
     adjusted_frame = cv2.convertScaleAbs(frame, alpha=CONTRAST, beta=BRIGHTNESS)
 
     # YOLO 추론
-    results = model(frame, conf=CONFIDENCE_THRESHOLD, verbose=False)
+    results = model(adjusted_frame, conf=CONFIDENCE_THRESHOLD, verbose=False)
 
     # 결과 화면에 그리기
     annotated_frame = results[0].plot()
@@ -79,7 +82,7 @@ while True:
         print(f"📸 사진이 저장되었습니다: {filename}")
 
     # 'q' 키로 종료
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if key == ord('q'):
         break
 
 # 종료 정리
